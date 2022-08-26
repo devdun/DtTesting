bb6e8b42e0ae23e4d7f00781640b12395443d483
Devdun <devdun90@gmail.com>
2022-08-26 23:59:14 +0530

    xpaths and pagefactory

--- src/main/java/pageFactory/toDo/ToDoPage.java
+++ src/main/java/pageFactory/toDo/ToDoPage.java
@@ -1,5 +1,6 @@
 package pageFactory.toDo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,22 +8,66 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.CommonOperations;
 
+import java.util.List;
+
+
 public class ToDoPage {
     public WebDriver driver;
-    private CommonOperations commonOperations;
 
     public ToDoPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
-        commonOperations = new CommonOperations(null);
     }
 
    @FindBy(xpath = ToDoXpathContents.TODO_INPUT)
     private WebElement todoInput;
 
+    @FindBy(partialLinkText = ToDoXpathContents.ALL_LINK_TEXT)
+    private WebElement allListBtn;
+
+    @FindBy(partialLinkText = ToDoXpathContents.ACTIVE_LINK_TEXT)
+    private WebElement activeListBtn;
+
+    @FindBy(partialLinkText = ToDoXpathContents.COMPLETED_LINK_TEXT)
+    private WebElement completedListBtn;
+
+    @FindBy(partialLinkText = ToDoXpathContents.CLEAR_COMPLETED_LINK_TEXT)
+    private WebElement clearCompletedListBtn;
+
+    @FindBy(partialLinkText = ToDoXpathContents.ITEM_LEFT_COUNT)
+    private WebElement itemLeftCount;
+
     public void setTodo(String todoText){
         todoInput.clear();
         todoInput.sendKeys(todoText);
         todoInput.sendKeys(Keys.ENTER);
+    }
+
+    public void clickAllBtn(){
+        allListBtn.click();
+    }
+
+    public void clickActiveBtn(){
+        activeListBtn.click();
+    }
+
+    public void clickCompletedBtn(){
+        completedListBtn.click();
+    }
+
+    public void clickClearCompletedBtn(){
+        clearCompletedListBtn.click();
+    }
+
+    public void clickAllCheckboxes(){
+        List<WebElement> checkboxes = driver.findElements(By.xpath("//li//input[@type='checkbox']"));
+        for (WebElement checkbox : checkboxes) {
+//            String value = checkboxes.get(i).getAttribute("value");
+            checkbox.click();
+        }
+    }
+
+    public String getItemLeftCount(){
+        return itemLeftCount.getText();
     }
 }
--- src/main/java/pageFactory/toDo/ToDoXpathContents.java
+++ src/main/java/pageFactory/toDo/ToDoXpathContents.java
@@ -2,4 +2,9 @@ package pageFactory.toDo;
 
 public class ToDoXpathContents {
     public static final String TODO_INPUT = "//header/input";
+    public static final String ITEM_LEFT_COUNT = "//span[@class='todo-count']//strong";
+    public static final String ALL_LINK_TEXT= "All";
+    public static final String ACTIVE_LINK_TEXT= "Active";
+    public static final String COMPLETED_LINK_TEXT= "Completed";
+    public static final String CLEAR_COMPLETED_LINK_TEXT= "Clear completed";
 }
