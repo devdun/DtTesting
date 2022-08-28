02e36df919abcce38e1921b4834d10c3ca71e1a8
Devdun <devdun90@gmail.com>
2022-08-28 18:16:32 +0530

    Extent Report configuration

--- src/test/java/Runners/CucumberRunnerTest.java
+++ src/test/java/Runners/CucumberRunnerTest.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"src/test/resources/ToDoList.feature" },
         glue = {"tests/StepDefinition"},
-        plugin = { "json:target/cucumber-json-report.json","html:target/site/cucumber-pretty"},
+        plugin = { "json:target/cucumber-json-report.json","html:target/site/cucumber-pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 //        tags = "@ClearCompletedItem"
         tags = "@Create or @Edit or @CreateMultiple or @Complete or @RemoveIncompleteItem or @RemoveCompletedItem or @AllActiveCompleted or @ClearCompletedItem"
         )
